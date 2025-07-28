c9f1b1d481de9a6b2ddbd78a02c870402161c252
Marco-Rocco <marcorocco28@icloud.com>
2025-07-28 12:51:31 +0200

    feat: create info boxes for color docs

--- src/Stories/Colors.stories.tsx
+++ src/Stories/Colors.stories.tsx
@@ -31,15 +31,18 @@ export const Primary: Story = {
 
             .info{
             padding: var(--spacing-md);
-            background-color: var(--color-gray-5);
-            color: var(--color-gray-10);
+            background-color: var(--color-neutral-bcg);
+            color: var(--color-neutral-text);
             margin: var(--spacing-sm);
             text-align: center;
-            border: 1px solid var(--color-gray-10);
+            border: 1px solid var(--color-neutral-accent);
             }
             `}
                 </style>
                 <h1>Colors</h1>
+
+                <p>Click on a color to copy the value</p>
+                <h2>Primitives</h2>
                 <h3>Grays</h3>
                 <div className="container grays">
                     {Array.from({ length: 10 }).map((_, index) => (
@@ -78,6 +81,55 @@ export const Primary: Story = {
 
                     ))}
                 </div>
+
+                <h2>Semantic</h2>
+                <h3>Neutrals</h3>
+                <p className="info">Per questa rappresentazione abbiamo usato: {""}
+                    <ClickToCopy value={`var(--color-neutral-bcg)`} >
+                        --color-neutral-bcg
+                    </ClickToCopy>
+                    ,
+                    <ClickToCopy value={`var(--color-neutral-text)`} >
+                        --color-neutral-text
+                    </ClickToCopy>
+                    e
+                    <ClickToCopy value={`var(--color-neutral-accent)`} >
+                        --color-neutral-accent
+                    </ClickToCopy>
+                </p>
+
+                <h3>Positives</h3>
+                <p className="info"
+                    style={{ backgroundColor: 'var(--color-positive-bcg)', color: 'var(--color-positive-text)', borderColor: 'var(--color-positive-accent)' }}>
+                    Per le rappresentazioni in positivo utilizzeremo: {""}
+                    <ClickToCopy value={`var(--color-positive-bcg)`} >
+                        --color-positive-bcg
+                    </ClickToCopy>
+                    ,
+                    <ClickToCopy value={`var(--color-positive-text)`} >
+                        --color-positive-text
+                    </ClickToCopy>
+                    e
+                    <ClickToCopy value={`var(--color-positive-accent)`} >
+                        --color-positive-accent
+                    </ClickToCopy>
+                </p>
+
+                <h3>Negatives</h3>
+                <p className="info"
+                    style={{ backgroundColor: 'var(--color-negative-bcg)', color: 'var(--color-negative-text)', borderColor: 'var(--color-negative-accent)' }}>Per le rappresentazioni in negativo utilizzeremo: {""}
+                    <ClickToCopy value={`var(--color-negative-bcg)`} >
+                        --color-negative-bcg
+                    </ClickToCopy>
+                    ,
+                    <ClickToCopy value={`var(--color-negative-text)`} >
+                        --color-negative-text
+                    </ClickToCopy>
+                    e
+                    <ClickToCopy value={`var(--color-negative-accent)`} >
+                        --color-negative-accent
+                    </ClickToCopy>
+                </p>
             </div>
         </>
     )
