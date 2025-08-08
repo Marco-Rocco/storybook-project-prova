57bf24a418436ccf638a7e81b76df03878966e2b
Marco-Rocco <marcorocco28@icloud.com>
2025-08-08 16:49:57 +0200

    feat: finish input and add styles + install feather

--- src/Stories/Input.stories.tsx
+++ src/Stories/Input.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Input } from "../components/Input";
+import { Input } from "../components/Input.tsx";
 
 const meta: Meta = {
     title: "Components/Input",
@@ -8,7 +8,9 @@ const meta: Meta = {
     },
     args: {
         label: "Label",
-    },
+        isInvalid: false,
+        disabled: false
+    }
 };
 
 export default meta;
@@ -16,5 +18,63 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+    render: ({ label, isInvalid, disabled }) => (
+        <div>
+            <Input disabled={disabled} isInvalid={isInvalid} errorText="Invalid email" label={label} iconPosition="right" kind="email" icon="Book" />
+            <Input isInvalid={isInvalid}
+                label={label}
+                options={[
+                    { label: "Option 1", value: "1" },
+                    { label: "Option 2", value: "2" },
+                ]}
+                kind="select"
+                disabled={disabled}
+            />
+            <Input isInvalid={isInvalid}
+                label={label}
+                options={[
+                    { label: "Option 1", value: "1" },
+                    { label: "Option 2", value: "2" },
+                ]}
+                kind="radio"
+                name="Mimmo"
+                disabled={disabled}
+                errorText="Invalid selection"
+            />
+        </div>
+    )
+};
+
+export const text: Story = {
     render: ({ label }) => <Input label={label} kind="text" />,
+};
+
+export const Select: Story = {
+    render: ({ label, isInvalid, disabled }) => (
+        <Input isInvalid={isInvalid}
+            label={label}
+            options={[
+                { label: "Option 1", value: "1" },
+                { label: "Option 2", value: "2" },
+            ]}
+            kind="select"
+            disabled={disabled}
+        />
+    ),
+};
+
+export const Radio: Story = {
+    render: ({ label, isInvalid, disabled }) => (
+        <Input isInvalid={isInvalid}
+            label={label}
+            options={[
+                { label: "Option 1", value: "1" },
+                { label: "Option 2", value: "2" },
+            ]}
+            kind="radio"
+            name="Mimmo"
+            disabled={disabled}
+            errorText="Invalid email"
+        />
+    ),
 };
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,96 @@
-export const Input = () => {
-    return <input type="text" placeholder="Type here..." />;
-}
+import React from "react";
+import root from "react-shadow";
+import cssText from "../styles/input.css?inline";
+import * as Icon from 'react-feather';
+import { GlobalStyles } from "../styles/GlobalStyles";
+
+type InputProps = {
+    kind: "text" | "email" | "password";
+    icon?: keyof typeof Icon;
+    iconPosition?: "left" | "right";
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+type SelectProps = {
+    kind: "select";
+    options: { label: string; value: string }[];
+    placeholder?: string;
+    icon?: never;
+    iconPosition?: never;
+} & React.SelectHTMLAttributes<HTMLSelectElement>;
+
+type RadioProps = {
+    kind: "radio";
+    options: { label: string; value: string }[];
+    placeholder?: never;
+    name: string;
+    icon?: never;
+    iconPosition?: never;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+type GeneralInputProps = (InputProps | SelectProps | RadioProps) & {
+    label: React.ReactNode;
+    icon?: keyof typeof Icon;
+    errorText?: React.ReactNode;
+    isInvalid?: boolean;
+};
+
+export const InternalInput: React.FC<InputProps | SelectProps | RadioProps> = (props) => {
+    switch (props.kind) {
+        case "select":
+            return (
+                <select {...props}>
+                    {props.placeholder && (
+                        <option value="" disabled selected>
+                            {props.placeholder}
+                        </option>
+                    )}
+                    {props.options.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </select>
+            );
+        case "radio":
+            return (
+                <>
+                    {props.options.map((option) => (
+                        <label key={option.value}>
+                            <input type="radio" {...props} value={option.value} />
+                            {option.label}
+                        </label>
+                    ))}
+                </>
+            );
+        default:
+            return <input type={props.kind} {...props} />;
+    }
+};
+
+export const Input: React.FC<GeneralInputProps> = ({ label, id, className, icon, iconPosition, errorText, isInvalid, ...props }) => {
+    const defaultId = React.useId() || id;
+    const IconComponent = icon ? Icon[icon] : null
+
+    return (
+        <root.div>
+            <GlobalStyles />
+            <style>{cssText}</style>
+            <div className={`${className ?? ""} container ${props.kind} ${isInvalid ? "is-invalid" : ""}`}>
+                {props.kind === "radio" ? (
+                    <span className="label">{label}</span>
+                ) : (
+                    <label htmlFor={defaultId}>{label}</label>
+                )}
+                <div className="input-container">
+                    <InternalInput {...props} id={defaultId} />
+                    {IconComponent && (
+                        <div className={`icon ${iconPosition || ""}`}>
+                            <IconComponent />
+                        </div>
+                    )}
+                </div>
+                {errorText && <span className="error-text">{errorText}</span>}
+            </div>
+        </root.div>
+    );
+};
